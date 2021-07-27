9199cd12c8ea11fed53824c44a2c6ddea9046539
Wakita <wakita.904@gmail.com>
2021-07-27 21:36:43 +0900

    todomodel_migrate

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class TodoModel(models.Model):
+  title = models.CharField(max_length=50)
+  content = models.TextField()
+  duedate = models.DateField()
