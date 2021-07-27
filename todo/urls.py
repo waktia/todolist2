a590495d118650c9dc28fb918382a53fa5024caa
Wakita <wakita.904@gmail.com>
2021-07-27 22:05:43 +0900

    list_create

--- todo/urls.py
+++ todo/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import Todolist
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
+    path("list/", Todolist.as_view(), name="list"),
 
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,11 @@
+from todo.models import TodoModel
 from django.shortcuts import render
+from .models import TodoModel
+from django.views.generic import ListView
+
 
 # Create your views here.
+
+class Todolist(ListView):
+  template_name = "list.html"
+  model = TodoModel
