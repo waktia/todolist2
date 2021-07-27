099cea0e80f1b6a7a0bc15805eaed962543b9c8f
Wakita <wakita.904@gmail.com>
2021-07-27 23:13:59 +0900

    new_create

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,8 @@
 from todo.models import TodoModel
 from django.shortcuts import render
 from .models import TodoModel
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 
 
 # Create your views here.
@@ -9,3 +10,9 @@ from django.views.generic import ListView
 class Todolist(ListView):
   template_name = "list.html"
   model = TodoModel
+
+class TodoCreate(CreateView):
+  template_name= "create.html"
+  model = TodoModel
+  fields = ("title", "content", "duedate")
+  success_url = reverse_lazy("list")
